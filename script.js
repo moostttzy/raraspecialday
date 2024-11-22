755bee9af471f8ba09705388b22f8ba6aa497b85
moostttzy <ahmdmsthf3@gmail.com>
2024-11-23 01:29:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 function showMainContent() {
-    console.log("Button clicked!"); // Debugging
+    console.log("Button clicked!"); // Debug
     document.getElementById("landing-page").classList.add("hidden");
     document.getElementById("main-content").classList.remove("hidden");
 }
